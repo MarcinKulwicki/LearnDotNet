8cb4d89b84dd300bd5897d87488263b80788bba2
Developer <MarcinKulwicki@protonmail.com>
2020-01-11 14:37:35 +0100

    End of Section 7

--- LearnDotNet-SPA/src/app/app.component.ts
+++ LearnDotNet-SPA/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthService } from './_services/auth.service';
 
